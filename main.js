f24eafaa4b99bc78ff40ea3d13a7a88a9e845985
jzimmerman95 <jessicazimmerman95@gmail.com>
2016-01-21 10:09:15 -0500

    everything done except contact

---
+++ main.js
@@ -0,0 +1,7 @@
+function colorBird() {
+  document.getElementById("bird").src = 'images/colorbird.png';
+}
+
+function grayscaleBird() {
+	document.getElementById("bird").src = 'images/bwbird.png';
+}
